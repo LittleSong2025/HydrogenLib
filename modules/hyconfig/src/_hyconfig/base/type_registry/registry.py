42ab4bcc13bbb25044270308796ac9dd1f72b10b
LittleSong2024 <idesong6@qq.com>
2025-08-27 15:45:15 +0800

    refactor(hyconfig): 重构类型注册表并添加新功能
    - 为 TypeRegistry 类添加泛型参数，提高类型安全性和灵活性
    - 添加 __getitem__、__setitem__ 和 __delitem__ 方法，支持索引操作
    - 优化 validate 方法的逻辑，提高代码可读性
    - 更新类型注解，提高代码的类型安全性

--- modules/hyconfig/src/_hyconfig/base/type_registry/registry.py
+++ modules/hyconfig/src/_hyconfig/base/type_registry/registry.py
@@ -5,31 +5,41 @@ from _hycore.utils import InstanceMapping
 type Validator = Callable[[Any], Any]
 
 
-class TypeRegistry:
+class TypeRegistry[_TS, _TT=_TS]:
     def __init__(self, mro=None):
-        self._mro = mro or InstanceMapping()  # type: InstanceMapping[type, InstanceMapping[type, Validator]]
+        self._mro = mro or InstanceMapping()  # type: InstanceMapping[type[_TS], InstanceMapping[type[_TT], Validator]]
 
-    def validate(self, data, target: type):
+    def validate(self, data: object, target: type[_TT]):
         sources = type(data).__mro__
         for source in sources:
             if self.exists(source, target):
                 return self.get(source, target)(data)
+        return None
 
-    def register(self, source: type, target: type, method: Validator):
+    def register(self, source: type[_TS], target: type[_TT], method: Validator):
         if source not in self._mro:
             self._mro[source] = InstanceMapping()
 
         self._mro[source][target] = method
 
-    def get(self, source: type, target: type):
+    def get(self, source: type[_TS], target: type[_TT]) -> Validator:
         if not self.exists(source, target):
             return None
         return self._mro[source][target]
 
-    def unregister(self, source: type, target: type):
+    def unregister(self, source: type[_TS], target: type[_TT]):
         if not self.exists(source, target):
             return
         del self._mro[source][target]
 
-    def exists(self, source: type, target: type):
+    def exists(self, source: type[_TS], target: type[_TT]):
         return source in self._mro and target in self._mro[source]
+
+    def __getitem__(self, item: tuple[type[_TS], type[_TT]]):
+        return self.get(*item)
+
+    def __setitem__(self, key: tuple[type[_TS], type[_TT]], value: Validator):
+        self.register(*key, value)
+
+    def __delitem__(self, key: tuple[type[_TS], type[_TT]]):
+        self.unregister(*key)
