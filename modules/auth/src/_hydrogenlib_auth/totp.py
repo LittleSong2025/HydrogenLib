09f463db29d0a4a2d5cd2387ae09871d9a92bc44
LittleSong2024 <idesong6@qq.com>
2025-10-05 17:09:31 +0800

    feat(auth): 实现TOTP和HOTP双因素认证功能
    - 添加了基于时间的一次性密码(TOTP)生成逻辑
    - 实现了基于HMAC的一次性密码(HOTP)生成
    - 支持可配置的哈希算法和时间周期

--- modules/auth/src/_hydrogenlib_auth/totp.py
+++ modules/auth/src/_hydrogenlib_auth/totp.py
@@ -1,5 +1,34 @@
+import hmac
+import struct
+
+import time
+
+from _hydrogenlib_core.hash import get_hash_func
+
+
 class TOTP:
-    def __init__(self, key, digits=6, period=30):
-        self.key = key
+    def __init__(self, secret_key, digits=6, period=30, hash_method: str = 'sha256'):
+        self.secret_key = secret_key
         self.digits = digits
         self.period = period
+        self.hash_method = get_hash_func(hash_method)
+
+    def generate(self, timestamp=None):
+        if timestamp is None:
+            timestamp = int(time.time())
+
+        timestamp = timestamp // self.period
+        ts_bytes = struct.pack('>Q', timestamp)
+        hash_value = self.hash_method(ts_bytes + self.secret_key).digest()
+        return int.from_bytes(hash_value[-4:], 'big') % 10 ** self.digits
+
+
+class HOTP(TOTP):
+    def generate(self, timestamp=None):
+        if timestamp is None:
+            timestamp = int(time.time())
+
+        timestamp //= self.period
+        ts_bytes = struct.pack('>Q',  timestamp)
+        hash_value = hmac.new(self.secret_key, ts_bytes, self.hash_method)
+        return int.from_bytes(hash_value.digest(), 'big') % 10 ** self.digits
